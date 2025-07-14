3694d544b3a0c374b3e872c01bd3b9ba77091e22
Kimsumin123 <tntn211@naver.com>
2025-07-14 20:46:15 +0900

    국기 추가

--- models/quiz_country.js
+++ models/quiz_country.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      img: {
+      flag: {
         type: DataTypes.STRING,
         allowNull: true,
       },
